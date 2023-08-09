483f7a72d801f8ecfb236f63bf176e1e990f6162
Stefan Prosiegel <stefan.prosiegel@googlemail.com>
2023-08-09 09:21:49 +0200

    add greifenklau, neder and hetzi

--- src/store/blog-entries.ts
+++ src/store/blog-entries.ts
@@ -1,4 +1,11 @@
-export const allBeerTypes = ['Lager', 'Kellerbier', 'Märzen', 'Rotbier', 'Dunkel'] as const
+export const allBeerTypes = [
+  'Lager',
+  'Kellerbier',
+  'Märzen',
+  'Rotbier',
+  'Dunkel',
+  'Helles'
+] as const
 export type BeerType = (typeof allBeerTypes)[number]
 
 export type BlogEntry = {
@@ -60,5 +67,32 @@ Insgesamt ein gelungenes Lager, das sich relativ süffig trinken und genießen l
     description: 'Dunkles mit bitterem Abgang - man lernt nie aus!',
     content: `Die Nase wird nach dem obligatorischen Flaschen-Riechtest von kräftigen Malznoten begrüßt, was bei einem Dunklen immer zu erwarten ist. <br /> Im Glas zeigt sich ein kastanienbraunes, dunkles Bier mit oppulenter Schaumrone, welche leider nicht lange anhält. Beim ersten Schluck offenbart sich zum initial gerochenen, dominanten Malzaroma allerdings eine ungeahnte Wendung:<br /> Nachdem die feinen Malzaromen den Gaumen hinabgeglitten sind, macht sich eine markante Bittere breit, welche kurz anhält und in einem wässrigen Abgang mündet.<br /> Sowas hab ich bei einem Dunklen in dem Ausmaß noch nicht im Glas, man lernt nie aus.<br /> Wer malzbetonte dunkle Biere verehrt, wird mit diesem Gebräu nur bedingt seine Freude haben, die Hopfenbittere in der zweiten Trinkhälfte ist für diesen Biertyp mehr als ungewohnt.`,
     image: 'dorn-dunkel.webp'
+  },
+  {
+    id: 'greifenklau-keller',
+    date: '2023-08-05',
+    type: 'Kellerbier',
+    title: 'Greifenklau Kellerbier',
+    description: 'Solide Bamberger Kellerbier-Angelegenheit',
+    content: `Im Glas präsentiert sich ein relativ helles, unfiltriertes Gebräu mit geringer Spundung.<br /> Die Nase wird mit reichlich Malzaromen begrüßt und so geht es auch am Gaumen weiter.<br />Das Greifenklau Kellerbier ist eine leicht trinkbare, lockere Angelegenheit. Schnörkellos und ohne Zicken läuft es in die ohnehin verwöhnte fränkische Biertrinkerkehle hinunter, ohne einen signifikant-speziellen Eindruck zu hinterlassen.`,
+    image: 'greifenklau-keller.webp'
+  },
+  {
+    id: 'neder-keller',
+    date: '2023-08-06',
+    type: 'Kellerbier',
+    title: 'Neder Kellerbier',
+    description: 'Scheckt, wie es das Etikett verspricht!',
+    content: `Die Brauerei Neder aus Forchheim, weithin für die Schwarze Anna bekannt, stellt sich heute mit ihrem Kellerbier vor.<br />Im Glas zeigt sich eine typische Kellerbier-Flüssigkeit: Unfiltriert, goldfarben und mit vernachlässigbarer Spundung.<br />In der Nase wird allerdings schon das erste Ausrufezeichen gesetzt:<br /> Hier ist deutlich kräftigeres Malzaroma am Start, als bei manch anderem Konkurrenten.<br />Das Bier macht einen klassichen, kräftigen Eindruck und auch der Hopfen wurde nicht vergessen.<br />Nachdem die Malzigkeit vorbeigeglitten ist, folgt ein erkennbar bitterer Abgang.<br />Dieses Bier ist eher an den Genießer adressiert, der kräftige Geschmack bei Malz und Hopfen mindert eine zügige Schüttability.`,
+    image: 'neder-keller.webp'
+  },
+  {
+    id: 'hetzelsdorfer',
+    date: '2023-08-08',
+    type: 'Helles',
+    title: 'Hetzelsdorfer Vollbier',
+    description: 'Ein Vollbier, das nach Weizen riecht?',
+    content: `Beim Schnüffeltest wäre ich bei einer Blindverkostung sicher darauf hereingefallen:<br /> Das muss ein Weizenbier sein! - Denkste, das ist ein bernsteinfarbenes Vollbier!?<br />Nachdem der nasale, hefige Irrtum verflossen ist, zeigt sich im Geschmack eine typische "altfränkische" Malzigkeit, das Bier macht allgemein in Nase und Gaumen einen klassischen Old School Eindruck.<br /> Im Abgang zeigt sich eine trockene Herbe, die das ganze abrundet.<br /> Das filtrierte, eher dunkel-bernsteinfarbige Bier mit geringer Spundung kann man gut zu einer fränkischen Brotzeit oder auch einfach so mal trinken.`,
+    image: 'hetzelsdorfer.webp'
   }
 ]
